cf2d0ebe4e205df0c199c001d89638e748efafdb
Denis Menéndez <denis.menendez.a@gmail.com>
2023-12-02 22:52:40 +0100

    Day 2 - F

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -1,8 +1,47 @@
 package day2
 
 class Day2 {
-    fun sumOfGameIds(input: List<String>): Int {
-        return 0
+    data class Cubes(val n: Int, val color: String)
+    fun sumOfGameIds(input: List<String>, loadedInfo: String): Int {
+        val info = extractCubes(loadedInfo).flatten()
+        input.ifEmpty { return 0 }
+        val game = input.first()
+        val gameId = extractGame(game)
+        val cubesSequence = extractCubes(game)
+
+        return validGame(cubesSequence, info, gameId)
+    }
+
+    private fun validGame(cubesSequence: List<List<Cubes>>, loadedInfo: List<Cubes>, gameId: Int): Int {
+        for (list in cubesSequence) {
+            for (cubes in loadedInfo) {
+                val match = list.any { it.color == cubes.color && it.n <= cubes.n }
+                if (!match)
+                    return 0
+            }
+        }
+        return gameId
+    }
+
+    //Game 1: 4 red, 8 green; 8 green, 6 red; 13 red, 8 green; 2 blue, 4 red, 4 green
+    fun extractCubes(input: String): List<List<Cubes>> {
+        val startIndex = input.indexOfFirst { it.toString() == ":" }
+        if (startIndex == -1) return emptyList()
+        val cubesBlock = input.substring(startIndex + 2, input.count())
+        val cubesInfo = cubesBlock.split("; ")
+
+        return cubesInfo.map {
+            it.split(", ").map {
+                it.split(" ").let {
+                    Cubes(it.first().toInt(), it.last())
+                }
+            }
+        }
+    }
+    fun extractGame(input: String): Int {
+        input.ifEmpty { return 0 }
+        val gameIndex = input.indexOfFirst { it.isDigit() }
+        return input[gameIndex].digitToInt()
     }
 
 }
--- src/test/kotlin/day2/Day2Test.kt
+++ src/test/kotlin/day2/Day2Test.kt
@@ -4,21 +4,24 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day2Test {
+
+    val loadedInfo = "Game 0: 12 red, 13 green, 14 blue"
+
     @Test
     fun emptyList() {
-        val total = Day2().sumOfGameIds(emptyList<String>())
+        val total = Day2().sumOfGameIds(emptyList<String>(), loadedInfo)
         val expected = 0
         Assertions.assertEquals(expected, total)
     }
     @Test
     fun emptyContent() {
-        val total = Day2().sumOfGameIds(listOf(""))
+        val total = Day2().sumOfGameIds(listOf(""), loadedInfo)
         val expected = 0
         Assertions.assertEquals(expected, total)
     }
     @Test
     fun possibleGame() {
-        val total = Day2().sumOfGameIds(listOf("Game 1: 13 blue"))
+        val total = Day2().sumOfGameIds(listOf("Game 1: 13 blue"), loadedInfo)
         val expected = 1
         Assertions.assertEquals(expected, total)
     }
