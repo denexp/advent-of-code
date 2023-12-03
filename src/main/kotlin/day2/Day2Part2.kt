140404572f0fb303f83de8ada03d414a8c1ca946
Denis Menéndez <denis.menendez.a@gmail.com>
2023-12-03 12:24:57 +0100

    Day 2 P2 - S

--- src/main/kotlin/day2/Day2Part2.kt
+++ src/main/kotlin/day2/Day2Part2.kt
@@ -5,4 +5,16 @@ class Day2Part2 {
         return 0
     }
 
+    fun fewestNumberOfCubes(cubeList: List<List<Day2.Cubes>>): List<Day2.Cubes> {
+        val cubes = cubeList.flatten().groupBy { it.color }
+        return cubes.mapNotNull{
+            var highestValue = 0
+            for (i in it.value) {
+                if (i.n > highestValue)
+                    highestValue = i.n
+            }
+            it.value.find { it.n == highestValue }
+        }
+    }
+
 }
