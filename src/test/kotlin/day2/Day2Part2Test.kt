43bf8b04bf8ba74f0dde6508260de4a6966895a3
Denis Menéndez <denis.menendez.a@gmail.com>
2023-12-03 11:38:11 +0100

    Day 2 P2 - F

---
+++ src/test/kotlin/day2/Day2Part2Test.kt
@@ -0,0 +1,15 @@
+package day2
+
+import FileManager.readLines
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day2Part2Test {
+    @Test
+    fun emptyList() {
+        val total = Day2Part2().sumOfPower(emptyList<String>())
+        val expected = 0
+        Assertions.assertEquals(expected, total)
+    }
+
+}
