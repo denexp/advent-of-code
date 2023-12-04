346bd7829910ff2fc59485d4ed8d69fbf27df9d6
Denis Menéndez <denis.menendez.a@gmail.com>
2023-12-04 19:40:09 +0100

    Day 3 - F

---
+++ src/test/kotlin/day3/Day3Test.kt
@@ -0,0 +1,19 @@
+package day3
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+    @Test
+    fun emptyList() {
+        val total = Day3().sumOfEngineNumbers(emptyList<String>())
+        val expected = 0
+        Assertions.assertEquals(expected, total)
+    }
+    @Test
+    fun emptyContent() {
+        val total = Day3().sumOfEngineNumbers(listOf(""))
+        val expected = 0
+        Assertions.assertEquals(expected, total)
+    }
+}
